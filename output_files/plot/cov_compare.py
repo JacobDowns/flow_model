f66d6633eab1c4295ffcc8ca504333ba63e8553c
Jacob Downs <jacob.downs@umontana.edu>
2019-04-15 16:02:46 -0600

    more stuff

--- output_files/plot/cov_compare.py
+++ output_files/plot/cov_compare.py
@@ -14,40 +14,21 @@ ax = plt.subplot(111)
 ts = np.loadtxt('output_files/inversions/cov1/prior/sigma_ts.txt')
 #x = np.loadtxt('output_files/inversions/cov1/prior/x.txt')
 x1 = np.loadtxt('output_files/inversions/cov1/posterior/x_new.txt')
-x2 = np.loadtxt('output_files/inversions/sensitivity1/posterior/x_new.txt')[:-5]
+x2 = np.loadtxt('output_files/inversions/cov2/posterior/x_new.txt')
 P1 = np.loadtxt('output_files/inversions/cov1/posterior/Pxx_new.txt')
-P2 = np.loadtxt('output_files/inversions/sensitivity1/posterior/Pxx_new.txt')
+P2 = np.loadtxt('output_files/inversions/cov2/posterior/Pxx_new.txt')
 v1 = np.sqrt(np.diag(P1))
-v2 = np.sqrt(np.diag(P2))[:-5]
-
-print(abs(x1 - x2).max())
-
-print(x1.shape)
-print(x2.shape)
-#quit()
-print("x1")
-print(x1[-5:])
-print(x1[-5:] - 2.*v1[-5:])
-print(x1[-5:] + 2.*v1[-5:])
-
-print("x2")
-print(x2[-5:])
-print(x2[-5:] - 2.*v2[-5:])
-print(x2[-5:] + 2.*v2[-5:])
-#plt.plot(ts, x, 'g')
+v2 = np.sqrt(np.diag(P2))
 
 
 plt.fill_between(ts, x1 - 2.0*v1, x1 + 2.0*v1, color = current_palette[0], alpha = 0.3)
 plt.fill_between(ts, x2 - 2.0*v2, x2 + 2.0*v2, color = current_palette[3], alpha = 0.45)
 
-#plt.plot(ts, x1 - 2.0*v1, 'b')
 plt.plot(ts, x1, color = 'k', lw = 6., ms = 10, marker = 'o', alpha = 1.)
-plt.plot(ts, x1, color = sns.color_palette()[0], lw = 4., label = "Default", ms = 7, marker = 'o', alpha = 1.)
+plt.plot(ts, x1, color = sns.color_palette()[0], lw = 4., label = "5th Order", ms = 7, marker = 'o', alpha = 1.)
 
 plt.plot(ts, x2, color = 'k', lw = 6., ms = 10, marker = 'o', alpha = 0.8)
-plt.plot(ts, x2, color = sns.color_palette()[3], lw = 4., label = "Sensitivity", ms = 7, marker = 'o', alpha = 0.8)
-#plt.plot(ts, x2 + 2.0*v2, 'r')
-
+plt.plot(ts, x2, color = sns.color_palette()[3], lw = 4., label = "UT", ms = 7, marker = 'o', alpha = 0.8)
 
 plt.grid(color='slategray', linestyle=':', linewidth=1)
 
@@ -58,5 +39,6 @@ plt.tight_layout()
 ticks = ax.get_xticks()
 ax.set_xticklabels([int(abs(tick / 1000.)) for tick in ticks])
 plt.legend()
-plt.savefig('sensitivity.png', dpi=400)    
+plt.savefig('cov_compare.png', dpi=400)
+
 plt.show()
